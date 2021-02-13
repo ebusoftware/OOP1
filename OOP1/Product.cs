0f74598f6fedf7efe77102a5a803f9997899e35d
ebusoftware <77747681+ebusoftware@users.noreply.github.com>
2021-02-13 20:27:30 +0300

    Add project files.

---
+++ OOP1/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class Product
+    {
+        //Kimlik numarası gibi düşünülebilir ID.
+        public int Id { get; set; }
+        //Hangi kategoriye ait.
+        public int CategoryId { get; set; }
+        //Ürün isimi.
+        public string ProductName { get; set; }
+        //Ürün birim fiyatı.
+        public double UnitPrice { get; set; }
+        //Ürün stok adedi.
+        public int UnitsInStock { get; set; }
+    }
+}
